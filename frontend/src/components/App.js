a5d681b3c99f25cf4fa0ad43c24a72a0e0d81ca3
Mahesh Mani <manimahesh1@gmail.com>
2020-12-29 12:15:30 -0800

    aa

---
+++ frontend/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { component } from "react";
+import { render } from "react-dom";
+
+export default class App extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+              <HomePage />
+            </div>
+        );
+    }
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
